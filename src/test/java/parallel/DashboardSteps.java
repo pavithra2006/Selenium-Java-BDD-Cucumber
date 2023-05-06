b9d41d6641523d33b44152b89025e7eae6724c04
pavithra2006 <pavi2098@gmail.com>
2023-05-06 11:51:39 +0530

    Code cleaning

--- src/test/java/parallel/DashboardSteps.java
+++ src/test/java/parallel/DashboardSteps.java
@@ -14,12 +14,13 @@ public class DashboardSteps {
     @When("I enter {string}")
     public void i_enter(String searchProduct) {
         DashboardPage.setValueInSearchBar(searchProduct, "Search bar");
-
     }
 
     @Then("Verify user redirected to {string} page")
     public void verify_user_redirected_to_page(String searchProduct) throws InterruptedException {
         Assertions.assertThat(DashboardPage.getCurrentPgTitle())
                 .contains(searchProduct);
+
+
     }
 }
